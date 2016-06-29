a63b228a85f3b6c75525f0be69d40f82e4be36c6
eransharv <eransharv@gmail.com>
2016-06-29 12:31:30 +0300

    add connections pooling support

--- python/hello.py
+++ python/hello.py
@@ -1,5 +1,13 @@
 import redis
 
-r = redis.StrictRedis(host='localhost', port=6379, db=0)
-r.set('foo', 'bar')
+print("connecting to redis labs cloud...")	
+
+pool = redis.ConnectionPool(host='pub-redis-13628.us-east-1-2.1.ec2.garantiadata.com', port=13628,password='rs87362c')
+r = redis.Redis(connection_pool=pool)
+
+##r = redis.StrictRedis(host='pub-redis-13628.us-east-1-2.1.ec2.garantiadata.com', port=13628,password='rs87362c')
+
+print("set test key...")
+r.set('eran', 'bar')
 r.get('foo')
+print("done.")
