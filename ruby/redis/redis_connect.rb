11973e911ed727671b0ffb3e6bd55c69451cf6b2
eransharv <eransharv@gmail.com>
2016-07-07 13:44:21 +0000

    add ruby connection pooling supprt

--- ruby/redis/redis_connect.rb
+++ ruby/redis/redis_connect.rb
@@ -1,8 +1,18 @@
 require 'redis'
+require 'connection_pool'
 
-redis = Redis.new(:host => "pub-redis-13628.us-east-1-2.1.ec2.garantiadata.com", :port => 13628, :db => 0, :password => "rs87362c")
-
-redis.set('foo', 'bar');
-value = redis.get('foo');
-puts value
+Redis.current = ConnectionPool.new(size: 10, timeout: 5) do
+	redis = Redis.new(
+		:host => "pub-redis-13628.us-east-1-2.1.ec2.garantiadata.com", 
+		:port => 13628, 
+		:db => 0, 
+		:password => "rs87362c",
+		:connect_timeout => 1.0,
+		:read_timeout => 1.0,
+		:write_timeout   => 1.0
+	)
+	redis.set('foo', 'bar');
+	value = redis.get('foo');
+	puts  value
+end
 
