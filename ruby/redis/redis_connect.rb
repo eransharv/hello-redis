9c8947da6f8ccf762872f8d939d517a52aeb5508
eransharv <eransharv@gmail.com>
2016-07-27 11:10:13 +0000

    add code comments

--- ruby/redis/redis_connect.rb
+++ ruby/redis/redis_connect.rb
@@ -1,7 +1,10 @@
-require 'redis'
-require 'connection_pool'
+require 'redis' #using the redis-rb, A Ruby client library for Redis
+require 'connection_pool' //Generic connection pooling for Ruby
 
+
+#If no object is available in the pool within :timeout seconds, it  will raise an error
 Redis.current = ConnectionPool.new(size: 10, timeout: 5) do
+ 	#All timeout values are specified in seconds
 	redis = Redis.new(
 		:host => "pub-redis-13628.us-east-1-2.1.ec2.garantiadata.com", 
 		:port => 13628, 
