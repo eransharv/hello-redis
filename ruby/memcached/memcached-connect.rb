dbff273c5fb0ec716309308009d1ea417317206c
eransharv <eransharv@gmail.com>
2016-08-28 09:05:31 +0000

    adding php folder

--- ruby/memcached/memcached-connect.rb
+++ ruby/memcached/memcached-connect.rb
@@ -1,4 +1,6 @@
-require 'dalli'
+require 'dalli' #Ruby client for accessing memcached servers
+require 'connection_pool' #Generic connection pooling for Ruby
+
 
 options = { :username => 'eran', :password => 'rs87362c' }
 dc = Dalli::Client.new('pub-memcache-11977.us-east-1-1.2.ec2.garantiadata.com:11977', options)
